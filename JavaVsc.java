4b3a4e1d3c1d1de8d0561969b58bebe3790b0d24
Lucas Politti <lucaspolitti7@gmail.com>
2022-11-16 22:25:49 -0300

    Teste de envio pelo terminal

--- JavaVsc.java
+++ JavaVsc.java
@@ -3,5 +3,10 @@ class JavaVsc {
 
 
         System.out.println("Olá GitHub!");
+
+        Double a = 3.5;
+        Double b = 4.0;
+
+        System.out.println("O valor de a + b é: " + (a+b));
     }
 }
